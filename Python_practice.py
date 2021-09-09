85c92ea685d68356fbd108bb1f3dee5b110162cf
velasciraptor <amyvelasquez432@gmail.com>
2021-09-09 15:42:54 -0500

    Update Python_practice.py

--- Python_practice.py
+++ Python_practice.py
@@ -63,4 +63,22 @@ for voters in counties_dict.values():
 for county, voters in counties_dict.items():
     print(county, voters)
 
-    
+# 3.2.11 F-Strings
+    # Using Concatentaion
+counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(county + " county has " + str(voters) + " registered voters.") 
+
+    # Using F-String
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")   
+
+    # Multi-line F-Strings
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+
+print(message_to_candidate) 
