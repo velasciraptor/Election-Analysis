e8bac57c6fe58b0f7f25c808de4946a06dcca51a
velasciraptor <amyvelasquez432@gmail.com>
2021-09-09 23:18:44 -0500

    3.6.4

--- PyPoll.py
+++ PyPoll.py
@@ -38,13 +38,23 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] += 1
 
-# 4. The total number of  votes each candidate won
+with open(file_to_save, "w") as txt_file:
+    election_results = (
+        f"\nElection Results\n"
+        f"----------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"----------------------------\n")
+    print(election_results, end="")
+    txt_file.write(election_results)
+    # 4. The total number of  votes each candidate won
     for candidate_name in candidate_votes:
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes) / float(total_votes) * 100
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results, end="")
+        txt_file.write(candidate_results)
 
-# 5. The winner of the eletion based on popular vote            
+    # 5. The winner of the eletion based on popular vote            
         if (votes > winning_count) and (vote_percentage > winning_percentage):
         # If true then set winning_count = votes and winning_percent =
         # vote_percentage.
@@ -52,12 +62,13 @@ with open(file_to_load) as election_data:
             winning_percentage = vote_percentage
         # And, set the winning_candidate equal to the candidate's name.
             winning_candidate = candidate_name
-             
-# Print the results
+
+    # Print the results
     winning_candidate_summary = (
     f"-------------------------\n"
     f"Winner: {winning_candidate}\n"
     f"Winning Vote Count: {winning_count:,}\n"
     f"Winning Percentage: {winning_percentage:.1f}%\n"
     f"-------------------------\n")
-print(winning_candidate_summary)        
+    print(winning_candidate_summary)       
+    txt_file.write(winning_candidate_summary)
