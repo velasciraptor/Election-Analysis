849db76da82d6678ecc1e1860060d0d421ffeca4
velasciraptor <amyvelasquez432@gmail.com>
2021-09-09 20:22:47 -0500

    3.4.5

--- PyPoll.py
+++ PyPoll.py
@@ -1,31 +1,19 @@
 # The data we need to retrieve.
 import csv 
 import os
-# Assign a variable for the file to load and the path.
-file_to_load = 'Resources\election_results.csv'
-
-# Open the election results and read the file
-with open(file_to_load) as election_data:
-
-     # To do: perform analysis.
-     print(election_data)
 
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
-# Open the election results and read the file.
-with open(file_to_load) as election_data:
-
-    # Print the file object.
-     print(election_data)
-
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
 
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
-    # Write three counties to the file.
-     txt_file.write("Arapahoe, Denver, Jefferson") 
-
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)    
 # 1. The total number of votes cast
 # 2. A complete list of candidates who received votes
 # 3. The percentage of votes each candidate won
